334bc026cce2fcfd751df96ba9e01251ed61c84f
Xinglu Wang <907682447@qq.com>
2018-05-27 14:58:44 +0800

    thesis: presentation&defense

---
+++ the.pre/guard.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# run make whenever something (anything, really) changes in the directory
+# needs inotifywait which is in
+# $ apt-get install inotify-tools
+while [ 1 ] ;  do
+  # inotifywait -e close_write,moved_to,create,modify thesis.tex |
+  # while read -r filename events; do
+  #   echo ">> GUARD: running make " ${events} ${filename} 
+  #   make
+  # done
+  make 
+  sleep 300
+done
